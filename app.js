49ee04f44143232e382e63542917924785208bb3
Yuji Kohatsu <111162170+kaikyuji@users.noreply.github.com>
2023-01-26 17:45:12 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,49 +1,36 @@
 const express = require('express')
+const body_parser = require('body-parser')
 const app = express()
-const bodyParser = require('body-parser')
 
-app.listen(3000, ()=>{console.log('Server is running on 3000')})
+app.use(body_parser.urlencoded({extended:true}))
 
-app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs')
-app.use('/public', express.static('public'))
-
 
 app.get('/', function(req, res){
-    let today = new Date();
+    var today = new Date();
+
+    var options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        hour: '2-digit',
+        minute: '2-digit'
+    }
     
-        let options = {
-            weekday: 'long',
-            day: 'numeric',
-            month: 'long',
-            hour: '2-digit',
-            minute: '2-digit'
-        }
-        let difGMT = today.getTimezoneOffset() / 60
-        if(difGMT > 0){
-            difGMT = '-' + difGMT
-        }else if(difGMT < 0){
-            difGMT = '+' + difGMT
-        }else{
-            difGMT = ' ' + difGMT
-        }
-        let datetime = today.toLocaleDateString('en-us', options)
-        console.log(datetime)
-    res.render('index', {newItemEJS:itemsTDL, Date: datetime, GMT: difGMT})
+    var day = today.toLocaleDateString('en-us', options)
+    app.use(express.static('public'))
+    res.render('list', {TypeOfday: day, newItem: items })
+
 })
-let itemsTDL = [] 
+
+var items = []
+
+app.listen(3000, function(){
+    console.log('Server is running on port 3000')
+})
 
 app.post('/', function(req, res){
-    itemsTDL.push(req.body.newItemJS)
-    res.redirect('/')
-    // não pode usar o render aqui, pois os forms estão no método POST, ou seja, os dados serão enviados aos POST requests.
-    // com isso, renderizando eles em todo post request, implica que estaremos na página onde os dados estão guardados
-    // o que não é o que queremos, queremos um site sem nenhum dado guardado, apenas requisições POST que recebem os dados e fazem alguma
-    // determinada ação com esses
-})
-
-app.post('/clear', function(req, res){
-    itemsTDL = []
+    items.push(req.body.newItem)
+    console.log(items)
     res.redirect('/')
 })
-
